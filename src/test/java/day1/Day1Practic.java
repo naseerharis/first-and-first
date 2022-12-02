1ee426e8553f395b5075f7fa01048b6eb10f7aed
naseerharis <naseer.haris@tekschool.us>
2022-12-01 21:09:04 -0800

    this third commit

--- src/test/java/day1/Day1Practic.java
+++ src/test/java/day1/Day1Practic.java
@@ -5,6 +5,7 @@ public class Day1Practic {
 	public static void main(String[] args) {
 		String name = "naseer";
 		String lastName = "haris";
+		
 
 	}
 	public void printName(){
@@ -15,4 +16,8 @@ public class Day1Practic {
 		return number;
 		// this is new method added on git.
 	}
-}
+		// this is new change on git
+				int a = 10000000;
+				double b = 1000.25;
+	}
+
